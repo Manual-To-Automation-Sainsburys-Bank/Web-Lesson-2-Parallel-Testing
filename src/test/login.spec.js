7967297f04e3f5716399032bcbb403f458f4ebea
Gary Behan <gary.b@browserstack.com>
2023-06-02 11:43:31 +0100

    Initial commit

---
+++ src/test/login.spec.js
@@ -0,0 +1,34 @@
+describe('Perform Login Operations', async () => {
+
+  beforeEach('Open Page', () => {
+    browser.url('https://practicetestautomation.com/practice-test-login/');
+  });
+  
+  it('Login Successfully', async () => {
+    await $("#username").setValue("student");
+    await $("#password").setValue("Password123");
+    await $("#submit").click();
+
+    await expect(browser).toHaveUrl("https://practicetestautomation.com/logged-in-successfully/");
+  });
+
+  it('Failed Login - Username', async () => {
+    await $("#username").setValue("wrongstudent");
+    await $("#password").setValue("Password123");
+    await $("#submit").click();
+
+    let errorElement = await $("#error");
+    await expect(errorElement).toHaveText("Your username is invalid!");
+  });
+
+  it('Failed Login - Password', async () => {
+    await $("#username").setValue("student");
+    await $("#password").setValue("WrongPassword123");
+    await $("#submit").click();
+
+    let errorElement = await $("#error");
+    await expect(errorElement).toHaveText("Your password is invalid!");
+  });
+})
+  
+  
